bff707cb31008dfb344bc1a75900c1d7d1266b1c
imreyes <guangyang11231987@gmail.com>
2017-01-07 22:04:49 -0500

    Solutions for more complicated real world problems of sequence finding

---
+++ RealWorldBindingSiteMatch.py
@@ -0,0 +1,289 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan  5 13:50:31 2017
+
+Function Gallery for Real-World DNA Sequence Problems.
+@author: Guang Yang
+"""
+import numpy as np
+import math
+
+# Print out lists.
+def prtout(text):
+    for i in text:
+        print(i, end = ' ')
+
+
+# DNA pattern to number
+def ptn(text):
+    def gtn(char):
+        if char=='A' or char=='a':
+            return 0
+        elif char=='C' or char=='c':
+            return 1
+        elif char=='G' or char=='g':
+            return 2
+        elif char=='T' or char=='t':
+            return 3
+    num=0
+    for char in text:
+        num=num*4+gtn(char)
+    return num
+
+
+# DNA code number to pattern.
+def ntp(num,k):
+    def ntg(num):
+        if num==0:
+            return 'A'
+        elif num==1:
+            return 'C'
+        elif num==2:
+            return 'G'
+        elif num==3:
+            return 'T'
+    seq=''
+    count=0
+    while count<k:
+        seq+=ntg(num%4)
+        num=num//4
+        count+=1
+    if num!=0:
+        print('Warning: residual number detected; code not fully transfered!')
+    return seq[::-1]
+
+
+# Finding Hamming distance between the two strings of equal lengths.
+# Specifically, finding distance between two genomes of same lengths.
+def hamd(genome1, genome2, d = -1):
+    if len(genome1) != len(genome2):
+        print("Error: strings differ in length!")
+        exit()
+    dist = 0
+    length = len(genome1)
+    for i in range(length):
+        if genome1[i] != genome2[i]:
+            dist += 1
+            if d >=0 and dist > d:
+                return dist
+    return dist
+
+
+# Find neighbors close in Hamming Distance d, of a given pattern.
+def neighbor(pattern, d):
+    nbhood = list()
+    cache = list()
+    seed = ['A', 'T', 'C', 'G']
+    for elem in seed:
+        nbhood.append(elem)
+    idx = 1
+    k = len(pattern)
+    while idx < k:
+        sub = pattern[0:idx]
+        for elem in nbhood:
+            if hamd(sub, elem, d) < d:
+                for nuc in seed:
+                    cache.append(elem + nuc)
+            else:
+                cache.append(elem + pattern[idx])
+        nbhood = cache
+        cache = list()
+        idx += 1
+    return nbhood
+   
+
+# Find k-mers that appear in all strings of DNA colletion.
+def checkpat(DNA, k, d):
+    def freqlist(genome, k, d):
+        flist = [0]*(4**k)
+        i = 0
+        n = len(genome)
+        while i <= n-k:
+            pattern = genome[i:(i+k)]
+            nbhood = neighbor(pattern, d)
+            for elem in nbhood:
+                flist[ptn(elem)] += 1
+            i += 1
+        return flist
+    fmat = np.zeros([len(DNA), 4**k])
+    for i, genome in enumerate(DNA):
+        fmat[i] = freqlist(genome, k, d)
+    checklist = np.ones(4**k)
+    for lines in fmat:
+        checklist *= lines
+    patterns = list()
+    for i, j in enumerate(checklist):
+        if j:
+            patterns.append(ntp(i, k))
+    patterns.sort()
+    return patterns
+
+
+# Calculate entropy.
+def entropy(DNA):
+    denom = len(DNA)
+    entropy = 0
+    for i in range(len(DNA[0])):
+        chars = {'A':0, 'T':0, 'C':0, 'G':0}
+        for genome in DNA:
+            chars[genome[i]] += 1/denom
+        print(chars)
+        for val in chars.values():
+            val = round(val, 1)
+            if val==0:
+                continue
+            entropy -= round(val * math.log(val, 2),5)      
+    return entropy
+
+
+# Find distance between designated sequence pattern
+# and strings of DNA.
+# By: summing minima of all distances between pattern and Dna strings.
+def dpatdna(pattern, DNA):
+    k = len(pattern)
+    distance = 0
+    for Dna in DNA:
+        n = len(Dna)
+        dist = k
+        i = 0
+        while i <= n - k:
+            d = hamd(pattern, Dna[i:(i+k)])
+            if dist > d:
+                dist = d
+            if dist == 0:
+                break
+            i += 1
+        distance += dist
+    return distance
+
+
+# Find median string that minimizes distance score.
+# The string has the smallest summed distances with all strings of DNA.
+# Input: DNA as list of strings containing genome (ATCG), k as designated k-mer to search,
+# greedy to find all (True) or only one (False).
+def medstr(DNA, k, greedy = False):
+    distance = math.inf
+    if greedy:
+        flist = [0] * (4**k)
+    if not greedy:
+        median = ''
+    for i in range(4**k):
+        pattern = ntp(i, k)
+        dist = dpatdna(pattern, DNA)
+        if greedy:
+            flist[i] = dist
+        if (not greedy) and distance > dist:
+            distance = dist
+            median = pattern
+    if not greedy:
+        return median
+    else:
+        m = min(flist)
+        return [ntp(i, k) for i, j in enumerate(flist) if j == m]
+
+
+# Find the most probable k-mer pattern in a DNA string,
+# given the profile probability of the k-mer.
+# Input: DNA as single string, integer k as designated k-mer to search,
+# profile as array, indicating probability for genome(ACGT, row-wise)
+# at each location of the k-mer (column).
+def profilemostk(DNA, k, profile):
+    n = len(DNA)
+    maxprob = 0
+    maxidx = 0
+    i = 0
+    def getidx(char):
+        if char == 'A':
+            return 0
+        elif char == 'C':
+            return 1
+        elif char == 'G':
+            return 2
+        elif char == 'T':
+            return 3
+    while i <= n - k:
+        pattern = DNA[i:(i+k)]
+        prob = 1
+        for j in range(k):
+            idx = getidx(pattern[j])
+            p = float(profile[idx][j])
+            if p == 0:
+                prob = 0
+                break
+            prob *= p
+        if maxprob < prob:
+            maxprob = prob
+            maxidx = i
+        i += 1
+    return DNA[maxidx:(maxidx+k)]
+
+
+# Return score of motif collection.
+def score(motifs):
+    k = len(motifs[0])
+    t = len(motifs)
+    scores = 0
+    for i in range(k):
+        chars = {'A':0, 'T':0, 'C':0, 'G':0}
+        for j in range(t):
+            char = motifs[j][i]
+            chars[char] += 1
+        scores += (t - max(chars.values()))
+    return scores
+
+
+# Return profile matrix of motif collection.
+# Columns represent ith genome position in the k-mer;
+# rows represent probability of 'A', 'C', 'G', 'T'.
+def profile(motifs):
+    k = len(motifs[0])
+    t = len(motifs)
+    mat = [[], [], [], []]
+    gdict = {'A':0, 'C':1, 'G':2, 'T':3}
+    for i in range(k):
+        chars = {'A':0, 'T':0, 'C':0, 'G':0}
+        for j in range(t):
+            char = motifs[j][i]
+            chars[char] += round(1/t, 3)
+        for key, val in chars.items():
+            mat[gdict[key]].append(val)
+    return mat
+
+
+# Upgrade the profile() function, applying Laplace's Rule.
+# When counting genome appearances in motifs, add the entire matrix by one.
+def laprofile(motifs):
+    k = len(motifs[0])
+    t = len(motifs)
+    mat = [[], [], [], []]
+    gdict = {'A':0, 'C':1, 'G':2, 'T':3}
+    for i in range(k):
+        chars = {'A':0, 'T':0, 'C':0, 'G':0}
+        for j in range(t):
+            char = motifs[j][i]
+            chars[char] += round(1/t, 3)
+        for key, val in chars.items():
+            mat[gdict[key]].append(val + 1/t)
+    return mat
+        
+
+# Search for best motif collection for the DNA collections.
+# Not an accurate solution.
+# Input: list of DNA strings, integer k for size of k-mer.
+def greedymotif(DNA, k, laplace = True):
+    t = len(DNA)
+    bestmotifs = []
+    for i in DNA:
+        bestmotifs.append(i[:k])
+    string1 = DNA[0]
+    for i in range(len(string1) - k + 1):
+        motifs = [string1[i:(i+k)]]
+        for j in range(1, t):
+            if laplace:
+                pro = laprofile(motifs)
+            else:
+                pro = profile(motifs)
+            motifs.append(profilemostk(DNA[j], k, pro))
+        if score(motifs) < score(bestmotifs):
+            bestmotifs = motifs
+    return bestmotifs
